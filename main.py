c15b8408a86b78bf18f47f2a3f5fc8be5acf3834
asharharoon <ashar_haroon2006@yahoo.com>
2021-06-12 15:02:38 +0500

    Add function to insert or delete an element from a certain index in an array.

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class Arrays:
                 print("Error! Elements are more than the size of the array.")
 
     """This function returns the number of the elements in an array, excluding the None values if there are any."""
+
     def my_len(self):
         length = 0
         for i in self.items:
@@ -28,6 +29,7 @@ class Arrays:
         return length
 
     """This function inserts the element at the last"""
+
     def insert_last(self, item):
         if self.my_len() < self.size:
             self.items.append(item)
@@ -35,14 +37,32 @@ class Arrays:
             print("Error! Element index out of range.")
 
     """This function inserts the element at the beginning of an array."""
+
     def insert_first(self, element):
         if self.my_len() < self.size:
             for i in range(self.my_len(), 0, -1):
-                self.items[i] = self.items[i-1]
+                self.items[i] = self.items[i - 1]
             self.items[0] = element
         else:
             print("Error! Element index out of range.")
 
-    def __str__(self):
+    """This function inserts the element at the given index in an array"""
+
+    def insert_at_index(self, index, element):
+        if (self.my_len()) < self.size:
+            for i in range(self.my_len(), index, -1):
+                self.items[i] = self.items[i - 1]
+            self.items[index] = element
+        else:
+            print("Error! Element index out of range.")
+
+    """This function removes the element at the given index in an array"""
+    def remove_at_index(self, index):
+        for i in range(index, self.my_len() - 1, 1):
+            self.items[i] = self.items[i + 1]
+        self.items[self.my_len()-1] = None
+
+    def print(self):
         print(self.items)
+
 
